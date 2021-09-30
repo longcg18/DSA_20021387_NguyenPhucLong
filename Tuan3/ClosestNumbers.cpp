9347e43860b2a97a67b19453a228cd1d47c8944a
Phuc Long Nguyen <20021387@vnu.edu.vn>
2021-09-30 22:23:36 +0700

    Update 30/9
    Update new algorithm for Pairs and ClosestNumbers

--- Tuan3/ClosestNumbers.cpp
+++ Tuan3/ClosestNumbers.cpp
@@ -1,5 +1,6 @@
+/**Complexity: O(nlogn)**/
 vector<int> closestNumbers(vector<int> arr) {
-    sort(arr.begin(), arr.end());
+    sort(arr.begin(), arr.end()); // nlogn
     int n = arr.size();
     int min_diff = arr[1] - arr[0];
     int diff[n];
@@ -10,7 +11,7 @@ vector<int> closestNumbers(vector<int> arr) {
             min_diff = diff[i];
         }
     }
-    cout << "min_diff = " << min_diff;
+    //cout << "min_diff = " << min_diff;
     vector<int> outPut;
     for (int i = 0; i < n - 1; i++) {
         if (diff[i] == min_diff) {
@@ -20,3 +21,26 @@ vector<int> closestNumbers(vector<int> arr) {
     }
     return outPut;
 }
+
+/**Complexity: O(nlogn) */
+bool myf(int i, int j) {
+    return i < j;
+}
+vector<int> closestNumbers(vector<int> arr) {
+    vector<int> closest;
+    sort(arr.begin(), arr.end()); //nlogn
+    int MIN = arr[1] - arr[0];
+    for (int i = 0; i < arr.size() - 1; i++) {
+        if (arr[i+1] - arr[i] < MIN) {
+            MIN = arr[i+1] - arr[i];
+        }
+    }
+    for (int i = 0; i < arr.size(); i++) {
+        int temp = arr[i] + MIN;
+        if (binary_search(arr.begin(), arr.end(), temp, myf)) {
+            closest.push_back(arr[i]);
+            closest.push_back(arr[i] + MIN);
+        }
+    }
+}
+
--- Tuan3/Pairs.cpp
+++ Tuan3/Pairs.cpp
@@ -1,8 +1,9 @@
+/**Complexity: O(nlogn) */
 int pairs(int k, vector<int> arr) {
-    sort(arr.begin(), arr.end());
+    sort(arr.begin(), arr.end()); //nlogn
     int cnt = 0;
     int i = 0, j = 1;
-    while(j < arr.size()) {
+    while(j < arr.size()) { //2n
         int diff = arr[j] - arr[i];
         if (diff == k) {
             cnt++;
@@ -14,4 +15,15 @@ int pairs(int k, vector<int> arr) {
         }
     }
     return cnt;
+}
+
+/**Complexity: O(n^2) */
+int pairs(int k, vector<int> arr) {
+    int temp = 0;
+    for (int i = 0; i < arr.size() - 1; i++){
+        for (int j = i + 1; j < arr.size(); j++){
+            if (abs(arr[i] - arr[j]) == k) temp++;
+        }
+    }
+    return t;
 }
